6ac5824f9f90535af3aa4654bc730caf72ce4958
tatamiii <sousuke2828@gmail.com>
2020-09-21 19:26:46 +0900

    newアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  get 'posts/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:index, :new]
 end
